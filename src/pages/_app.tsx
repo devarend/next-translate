166c85389a62a58e7b4c0d713dc798721851697d
devarend <devarend@gmail.com>
2023-02-12 21:01:50 +0100

    added type Translations, use context in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { Languages, TranslateContext } from "@/translate/TranslateContext";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{ language: Languages }>) {
+  return (
+    <TranslateContext language={pageProps.language || "en"}>
+      <Component {...pageProps} />
+    </TranslateContext>
+  );
 }
--- src/translate/TranslateContext.tsx
+++ src/translate/TranslateContext.tsx
@@ -3,17 +3,18 @@ import { translations } from "@/translate/translations";
 import { ReactElement, ReactFragment } from "react";
 
 const Context = createTypedContext({ translation: translations["en"] });
+export type Languages = keyof typeof translations;
 
 export const TranslateContext = ({
   children,
   language,
-}: TranslateContextProps) => {
+}: TranslateContextProps) => (
   <Context.Provider value={{ translation: translations[language] }}>
     {children}
-  </Context.Provider>;
-};
+  </Context.Provider>
+);
 
 interface TranslateContextProps {
   children: ReactElement | ReactFragment;
-  language: keyof typeof translations;
+  language: Languages;
 }
